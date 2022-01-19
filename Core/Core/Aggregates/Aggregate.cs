9f42bb837f6bfd8845879e10868510b83853a84c
Dong Zeng <dong@freshbyte.com>
2022-01-19 09:10:16 -0600

    starting Reservation feature

---
+++ Core/Core/Aggregates/Aggregate.cs
@@ -0,0 +1,34 @@
+﻿using Core.Events;
+
+namespace Core.Aggregates;
+
+public abstract class Aggregate : Aggregate<Guid>, IAggregate
+{
+}
+
+public abstract class Aggregate<T> : IAggregate<T> where T : notnull
+{
+    public T Id { get; protected set; } = default!;
+
+    public int Version { get; protected set; }
+
+    [NonSerialized] private readonly Queue<IEvent> uncommittedEvents = new();
+
+    public IEvent[] DequeueUncommittedEvents()
+    {
+        var dequeuedEvents = uncommittedEvents.ToArray();
+
+        uncommittedEvents.Clear();
+
+        return dequeuedEvents;
+    }
+
+    public void When(object @event)
+    {
+    }
+
+    protected void Enqueue(IEvent @event)
+    {
+        uncommittedEvents.Enqueue(@event);
+    }
+}
---
+++ Tickets/Config.cs
@@ -0,0 +1,23 @@
+﻿using Core.Marten;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickets;
+
+public static class Config
+{
+    public static void AddTicketsModule(this IServiceCollection services, IConfiguration config)
+    {
+        //services.AddMarten(config, options =>
+        //{
+        //    options.ConfigureReservations();
+        //});
+        //services.AddReservations();
+        //services.AddMaintainance();
+    }
+}
