eaeb157ed53e9b5d15843f769e0d411f4c5f32f8
Dong Zeng <dong@freshbyte.com>
2022-01-24 08:14:32 -0600

    GetReservationAtVersion and GetReservationById

--- Tickets/Reservations/GettingReservationAtVersion/GetReservationAtVersion.cs
+++ Tickets/Reservations/GettingReservationAtVersion/GetReservationAtVersion.cs
@@ -1,13 +1,47 @@
-﻿using Core.Queries;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Exceptions;
+using Core.Queries;
+using Marten;
 using Tickets.Reservations.GettingReservationById;
 
 namespace Tickets.Reservations.GettingReservationAtVersion;
 
 public class GetReservationAtVersion : IQuery<ReservationDetails>
 {
+    public Guid ReservationId { get; }
+    public int Version { get; }
+
+    private GetReservationAtVersion(Guid reservationId, int version)
+    {
+        ReservationId = reservationId;
+        Version = version;
+    }
+
+    public static GetReservationAtVersion Create(Guid reservationId, int version)
+    {
+        if (reservationId == Guid.Empty)
+            throw new ArgumentOutOfRangeException(nameof(reservationId));
+        if (version < 0)
+            throw new ArgumentOutOfRangeException(nameof(version));
+
+        return new GetReservationAtVersion(reservationId, version);
+    }
+}
+
+internal class HandleGetReservationAtVersion :
+    IQueryHandler<GetReservationAtVersion, ReservationDetails>
+{
+    private readonly IDocumentSession querySession;
+
+    public HandleGetReservationAtVersion(IDocumentSession querySession)
+    {
+        this.querySession = querySession;
+    }
+
+    public async Task<ReservationDetails> Handle(GetReservationAtVersion request, CancellationToken cancellationToken)
+    {
+        return await querySession
+            .Events
+            .AggregateStreamAsync<ReservationDetails>(request.ReservationId, request.Version, token: cancellationToken)
+               ?? throw AggregateNotFoundException.For<ReservationDetails>(request.ReservationId);
+    }
 }
---
+++ Tickets/Reservations/GettingReservationById/GetReservationById.cs
@@ -0,0 +1,25 @@
+﻿using Core.Exceptions;
+using Core.Queries;
+using Marten;
+
+namespace Tickets.Reservations.GettingReservationById;
+public record GetReservationById(
+    Guid ReservationId
+) : IQuery<ReservationDetails>;
+
+internal class HandleGetReservationById :
+    IQueryHandler<GetReservationById, ReservationDetails>
+{
+    private readonly IDocumentSession querySession;
+
+    public HandleGetReservationById(IDocumentSession querySession)
+    {
+        this.querySession = querySession;
+    }
+
+    public async Task<ReservationDetails> Handle(GetReservationById request, CancellationToken cancellationToken)
+    {
+        return await querySession.LoadAsync<ReservationDetails>(request.ReservationId, cancellationToken)
+               ?? throw AggregateNotFoundException.For<ReservationDetails>(request.ReservationId);
+    }
+}
