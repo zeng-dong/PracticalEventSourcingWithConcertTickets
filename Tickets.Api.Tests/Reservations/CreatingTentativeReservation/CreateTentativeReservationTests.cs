86eb6537b5d005f1bdedeabe5f5eab88c89ad44e
Dong Zeng <dong@freshbyte.com>
2022-01-26 08:36:23 -0600

    Core.Api.Testing lib

---
+++ Tickets.Api.Tests/Reservations/CreatingTentativeReservation/CreateTentativeReservationTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickets.Api.Tests.Reservations.CreatingTentativeReservation;
+
+//public class CreateTentativeReservationFixture : ApiWithEventsFixture<Startup>
+//{
+//
+//}
+
+public class CreateTentativeReservationTests
+{
+}
