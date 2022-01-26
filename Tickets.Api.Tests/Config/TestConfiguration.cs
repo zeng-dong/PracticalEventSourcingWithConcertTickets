2029366f0ac7797eb5621bb3d5ffc496c3e120bd
Dong Zeng <dong@freshbyte.com>
2022-01-26 07:48:20 -0600

    added integration test project

---
+++ Tickets.Api.Tests/Config/TestConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Tickets.Api.Tests.Config;
+
+public static class TestConfiguration
+{
+    public static Dictionary<string, string> Get(string fixtureName) =>
+        new Dictionary<string, string>
+        {
+            {
+                "EventStore:ConnectionString",
+                "PORT = 5432; HOST = localhost; TIMEOUT = 15; POOLING = True; MINPOOLSIZE = 1; MAXPOOLSIZE = 100; COMMANDTIMEOUT = 20; DATABASE = 'ConcertTickets'; PASSWORD = 'tester'; USER ID = 'tester'"
+            },
+            {"EventStore:WriteModelSchema", $"{fixtureName}Write"},
+            {"EventStore:ReadModelSchema", $"{fixtureName}Read"},
+            {"EventStore:ShouldRecreateDatabase", "true"}
+        };
+}
