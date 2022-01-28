dec97f16e3a37fcc746d236cdf084c8bd4994038
Dong Zeng <dong@freshbyte.com>
2022-01-28 08:41:43 -0600

    setting up integration test

--- Tickets.Api.Tests/Reservations/CreatingTentativeReservation/CreateTentativeReservationTests.cs
+++ Tickets.Api.Tests/Reservations/CreatingTentativeReservation/CreateTentativeReservationTests.cs
@@ -1,15 +1,33 @@
-﻿using System;
+﻿using Core.Testing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Tickets.Api.Tests.Config;
+
+using Tickets.Api.Requests;
 
 namespace Tickets.Api.Tests.Reservations.CreatingTentativeReservation;
 
-//public class CreateTentativeReservationFixture : ApiWithEventsFixture<Startup>
-//{
-//
-//}
+public class CreateTentativeReservationFixture : ApiWithEventsFixture<Startup>
+{
+    protected override string ApiUrl => "/api/Reservations";
+
+    protected override Dictionary<string, string> GetConfiguration(string fixtureName) =>
+        TestConfiguration.Get(fixtureName);
+
+    public readonly Guid SeatId = Guid.NewGuid();
+
+    public HttpResponseMessage CommandResponse = default!;
+
+    public override async Task InitializeAsync()
+    {
+        // send create command
+        CommandResponse = await Post(new CreateTentativeReservationRequest { SeatId = SeatId });
+    }
+}
 
 public class CreateTentativeReservationTests
 {
